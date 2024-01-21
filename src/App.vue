c015b5cca3b3ccde9da775684816332b2cc99b2e
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-21 13:50:45 +0530

    update: access store and render tasks.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
   <main>
     <header>
-      <img src="./assets//pinia-logo.svg" alt="pineapple" />
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Vue Todos</h1>
     </header>
+    <div v-for="task in tasks" :key="task.id" class="tasks">
+      <p>{{ task.id }}</p>
+      <p>
+        {{ task.title }}
+      </p>
+      <p>{{ task.isFav ? "fav" : "unfav" }}</p>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { useTaskStore } from "@/stores/TaskStore";
+import { storeToRefs } from "pinia";
+
+const taskStore = useTaskStore();
+
+const { tasks } = storeToRefs(taskStore);
+</script>
+
+<style>
+.tasks {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
