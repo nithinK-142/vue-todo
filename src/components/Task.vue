3bd0a44146e66d6cd87a527b0fd3043b6277f5d0
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-23 18:47:20 +0530

    add: Task.

---
+++ src/components/Task.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i @click="deleteTask(task.id)" class="material-icons">delete</i>
+      <i
+        @click="toggleFav(task.id)"
+        :class="{ active: task.isFav }"
+        class="material-icons"
+        >favorite</i
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useTaskStore } from "@/stores/TaskStore";
+
+const props = defineProps(["task"]);
+
+const taskStore = useTaskStore();
+const { deleteTask, toggleFav } = taskStore;
+</script>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -4,18 +4,7 @@
   <div class="loading" v-if="loading">Loading tasks...</div>
 
   <div v-for="task in filteredTasks" :key="task.id" class="task-list">
-    <div class="task">
-      <h3>{{ task.title }}</h3>
-      <div class="icons">
-        <i @click="deleteTask(task.id)" class="material-icons">delete</i>
-        <i
-          @click="toggleFav(task.id)"
-          :class="{ active: task.isFav }"
-          class="material-icons"
-          >favorite</i
-        >
-      </div>
-    </div>
+    <Task :task="task" />
   </div>
 </template>
 
@@ -25,11 +14,11 @@ import FilterButtons from "./FilterButtons.vue";
 import { storeToRefs } from "pinia";
 import { computed } from "vue";
 import TaskCount from "./TaskCount.vue";
+import Task from "./Task.vue";
 
 const taskStore = useTaskStore();
-const { getTasks, deleteTask, toggleFav } = taskStore;
 
-getTasks();
+taskStore.getTasks();
 
 const { filter, tasks, loading, favs } = storeToRefs(taskStore);
 
