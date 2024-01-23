d1007571660ad6375213c1cb3bbb0082c224afc1
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-23 18:57:13 +0530

    update: reset onclick pinia.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,19 @@
 <template>
   <header>
-    <img src="../assets/pinia-logo.svg" alt="pineapple" />
+    <a @click.prevent="taskStore.$reset">
+      <img src="../assets/pinia-logo.svg" alt="pineapple" />
+    </a>
     <h1>Vue Todos</h1>
   </header>
 </template>
+
+<script setup>
+import { useTaskStore } from "@/stores/TaskStore";
+const taskStore = useTaskStore();
+</script>
+
+<style scoped>
+a {
+  cursor: pointer;
+}
+</style>
